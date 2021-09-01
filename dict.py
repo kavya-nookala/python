129567b3980b6393a928524fe6930b1dfeadb189
kavya-nookala <89908530+kavya-nookala@users.noreply.github.com>
2021-09-01 18:01:57 +0530

    Add files via upload
    week1

---
+++ dict.py
@@ -0,0 +1,15 @@
+x={'apple':28,'mango':35,'grape':23,'banana':15}
+print(x)
+
+#length
+print(len(x))
+
+#access                        
+print(x.keys())
+print(x.values())
+print(x.items())
+
+#get
+y=x.get('mango')
+print(y)
+
---
+++ tuple.py
@@ -0,0 +1,14 @@
+t=(1,10,20,'hello')
+#add 
+#t[1]=5
+print(t)
+
+#length
+print(len(t))
+
+#check for any item
+print(any(t))
+
+#access
+print(t[0])
+print(t[1:3])
